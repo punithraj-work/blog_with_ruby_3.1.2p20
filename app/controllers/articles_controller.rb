eae31c7ccc8b6a70da104e68b80dea4736187900
punithraj-work <punithraj@dadutechnologies.com>
2022-07-13 20:38:54 +0530

    working till 7.4 partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,43 @@
 class ArticlesController < ApplicationController
   def index
-   @articles = Article.all
+    @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # def new1
+  #   @article = Article.all
+  # end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
+  # get "/articles", to: "articles#index"
+
+  # get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  resources  :articles
 end
